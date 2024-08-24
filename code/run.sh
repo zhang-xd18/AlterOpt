ef6e94abf4b9c435b894fd099e5669979ea9bc8d
zhang-xd18 <91357191+zhang-xd18@users.noreply.github.com>
2024-08-24 15:57:39 +0800

    Update run.sh

--- code/run.sh
+++ code/run.sh
@@ -1,15 +1,15 @@
-python /home/AlterOpt-main/code/main.py \
+python /home/code/main.py \
     --epochs 200 \
     --gpu 0 \
-    --root '/home/AlterOpt/' \
+    --root '/home/results/' \
     --name 'CRNet' \
-    --data-dir '/home/AlterOpt/Dataset/' \
+    --data-dir '/home/data/' \
     --batch-size 100 \
     --workers 0 \
     --cr 4 \
     --method 'Alter' \
     --scenarios 'CDADA' \
-    --pretrained '/home/AlterOpt/code/pretrain/ \
+    --pretrained '/home/code/pretrain/' \
     --store-num 100 \
     --period 2
 
