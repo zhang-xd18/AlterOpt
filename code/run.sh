28c2d6dd573571befb8d26c04a75cb2952757d39
zhang-xd18 <91357191+zhang-xd18@users.noreply.github.com>
2024-08-24 15:56:18 +0800

    Update run.sh

--- code/run.sh
+++ code/run.sh
@@ -1,30 +1,15 @@
-GPU=0
-NAME=CRNet
-ROOT=/teams/ius_1663576043/zxd/MagProject/
-DATA_DIR=$ROOT/3GPP
-DIR=$ROOT/results/
-PRETRAIN=$ROOT/AlterOpt-main/code/pretrain/
-BATCHSIZE=100
-CR=4
-
-METHOD=Alter
-EPOCH=200
-STORE_NUM=100
-PERIOD=2
-SCENARIOS=CDADA
-
-python $ROOT/AlterOpt-main/code/main.py \
-    --epochs $EPOCH \
-    --gpu $GPU \
-    --root $DIR \
-    --name $NAME \
-    --data-dir $DATA_DIR \
-    --batch-size $BATCHSIZE \
+python /home/AlterOpt-main/code/main.py \
+    --epochs 200 \
+    --gpu 0 \
+    --root '/home/AlterOpt/' \
+    --name 'CRNet' \
+    --data-dir '/home/AlterOpt/Dataset/' \
+    --batch-size 100 \
     --workers 0 \
-    --cr $CR \
-    --method $METHOD \
-    --scenarios $SCENARIOS \
-    --pretrained $PRETRAIN \
-    --store-num $STORE_NUM \
-    --period $PERIOD
+    --cr 4 \
+    --method 'Alter' \
+    --scenarios 'CDADA' \
+    --pretrained '/home/AlterOpt/code/pretrain/ \
+    --store-num 100 \
+    --period 2
 
