422f0144fb8058fea39f4569f14154c6df0a86b5
zhang-xd18 <91357191+zhang-xd18@users.noreply.github.com>
2024-10-17 16:13:03 +0800

    enrich comments

--- code/utils/scheduler.py
+++ code/utils/scheduler.py
@@ -3,7 +3,7 @@ from torch.optim.lr_scheduler import _LRScheduler
 
 __all__ = ['WarmUpCosineAnnealingLR', 'FakeLR']
 
-
+# cosine scheduler proposed by CRNet
 class WarmUpCosineAnnealingLR(_LRScheduler):
     def __init__(self, optimizer, T_max, T_warmup, eta_min=0, last_epoch=-1):
         self.T_max = T_max
@@ -18,7 +18,7 @@ class WarmUpCosineAnnealingLR(_LRScheduler):
             k = 1 + math.cos(math.pi * (self.last_epoch - self.T_warmup) / (self.T_max - self.T_warmup))
             return [self.eta_min + (base_lr - self.eta_min) * k / 2 for base_lr in self.base_lrs]
 
-
+# const scheduler
 class FakeLR(_LRScheduler):
     def __init__(self, optimizer):
         super(FakeLR, self).__init__(optimizer=optimizer)
